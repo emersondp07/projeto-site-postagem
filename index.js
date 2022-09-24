fd7203fe7b640bc71f579a5cc44422010bc5f0f3
Emerson Dantas <emerson.dantaspereira@hotmail.com>
2022-09-24 07:57:32 -0300

    Layout finalizado

--- index.js
+++ index.js
@@ -7,15 +7,27 @@
 // recuperando o modulo de configuração do server
 const app = require('./config/server');
 
+// recuperando o modulo de base de dados
+const noticias = require('./mockup');
+
 // rota home
 app.get('/', (req, res) => {
-    res.render('home/index')
+    res.render('home/index', { noticias: noticias.slice(0, 3) })
 });
 
 // rota notícias
 app.get('/noticias', (req, res) => {
-    res.render('news/noticias')
+    // passamos atravez de um JSON todas as noticias
+    res.render('news/noticias', { noticias: noticias })
 });
+
+//rota notícia
+app.get('/noticia', (req, res) => {
+    // recuperar atravez do método GET o ID
+    const id = req.query.id
+
+    res.render('news/noticia', { noticia: noticias[id] })
+})
 
 // rota admin
 app.get('/admin', (req, res) => {
