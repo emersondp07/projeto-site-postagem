ff9c2ae732925e8662998c9eb662c951dc55f911
Emerson Dantas <emerson.dantaspereira@hotmail.com>
2022-09-23 21:28:07 -0300

    Efeito barra de navegação

--- app/public/js/script.js
+++ app/public/js/script.js
@@ -1,1 +1,12 @@
 // Desenvolvido por Emerson
+
+// manipular nosso menu de navegação
+const url_atual = window.location.pathname
+
+if(url_atual == '/') {
+    document.getElementById('menuHome').className = 'nav-link text-whit active'
+} else if(url_atual == '/noticias') {
+    document.getElementById('menuNoticias').className = 'nav-link text-whit active'
+} else if(url_atual == '/admin') {
+    document.getElementById('menuAdmin').className = 'nav-link text-whit active'
+}
